41e6ab6d7efbb3baf7367c4ca9b1479149237ed8
Gabriel <gabrielnovatoo@gmail.com>
2023-01-11 15:56:07 -0300

    Create function of make new div w/ classes and btns in todo

--- js/script.js
+++ js/script.js
@@ -6,10 +6,41 @@ const editForm = document.querySelector("#edit-form");
 const editInput = document.querySelector("#edit-input");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 // Functions
+const saveTodo = (text) => {
+    const todo = document.createElement('div');
+    todo.classList.add('todo');
+
+    const todoTitle = document.createElement('h3');
+    todoTitle.innerText = text
+    todo.appendChild(todoTitle)
+
+    console.log(todo);
+
+    const doneBtn = document.createElement('button')
+    doneBtn.classList.add('finish-todo')
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
+    todo.appendChild(doneBtn)
+
+    const editBtn = document.createElement('button')
+    editBtn.classList.add('edit-todo')
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
+    todo.appendChild(editBtn)
+
+    const deleteBtn = document.createElement('button')
+    deleteBtn.classList.add('remove-todo')
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+    todo.appendChild(deleteBtn)
+
+    todoList.appendChild(todo)
+}
+
 
 // Events
 todoForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    console.log("Submit form")
+    const inputValue = todoInput.value;
+    if(inputValue){
+        saveTodo(inputValue)
+    }
 })
